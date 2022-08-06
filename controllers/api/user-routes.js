22c1d58fe4e4c541a510b9587d7984c58e0b5c22
Kevin Reid <kevinnivek@me.com>
2022-08-06 19:25:50 -0400

    signup form added

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,46 @@
+const router = require('express').Router();
+const { User, Post, Comment, Vote } = require('../../models');
+
+router.post('/login', (req, res) => {
+    // expects {username: 'GraceJones', password: 'password123'}
+    User.findOne({
+      where: {
+        username: req.body.username
+      }
+    }).then(dbUserData => {
+      if (!dbUserData) {
+        res.status(400).json({ message: 'No user with that email address!' });
+        return;
+      }
+  
+      const validPassword = dbUserData.checkPassword(req.body.password);
+  
+      if (!validPassword) {
+        res.status(400).json({ message: 'Incorrect password!' });
+        return;
+      }
+  
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+    
+        res.json({ user: dbUserData, message: 'You are now logged in!' });
+      });
+    });
+  });
+  
+  router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+      req.session.destroy(() => {
+        res.status(204).end();
+      });
+    }
+    else {
+      res.status(404).end();
+    }
+  });
+  
+  
+  
+  module.exports = router;
