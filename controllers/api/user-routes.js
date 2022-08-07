30a60cb12fae8a02908ad71a779c0686d6d6e1b7
Kevin Reid <kevinnivek@me.com>
2022-08-06 20:26:51 -0400

    signup working

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -40,6 +40,28 @@ router.post('/login', (req, res) => {
       res.status(404).end();
     }
   });
+
+  router.post('/signup', (req, res) => {
+    // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+    User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password
+    })
+      .then(dbUserData => {
+        req.session.save(() => {
+          req.session.user_id = dbUserData.id;
+          req.session.username = dbUserData.username;
+          req.session.loggedIn = true;
+    
+          res.json(dbUserData);
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
   
   
   
